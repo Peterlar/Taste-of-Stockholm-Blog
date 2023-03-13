417cf8fe18143885f9c72d24f00e4e9065918228
Peterlar <Peter_kjaer@hotmail.com>
2023-03-13 20:45:37 +0000

    Created superuser and added post and comments to adminsite

--- taste_of_stockholm/urls.py
+++ taste_of_stockholm/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from todo.views import say_hello
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', say_hello, name='hello')
+    path('summernote/', include('django_summernote.urls'))
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render,
 
 # Create your views here.
-
-
-def say_hello(request):
-    return HttpResponse("Hello")
